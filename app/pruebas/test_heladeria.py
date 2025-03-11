949c159893ea67b56237de0e0af4268ba36fc765
Gimandes <gimandes.ict@gmail.com>
2025-03-11 18:24:26 -0500

    Añadiendo archivos al branch nuevo_con_pruebas

---
+++ app/pruebas/test_heladeria.py
@@ -0,0 +1,33 @@
+import pytest
+from unittest.mock import MagicMock
+from sqlalchemy.orm import Session
+from models.ingrediente import Ingrediente
+from controllers.heladeria import es_sano, abastecer_ingrediente
+
+def test_es_sano():
+    assert es_sano(50, False) == True  
+    assert es_sano(150, True) == True  
+    assert es_sano(150, False) == False  
+
+def test_abastecer_ingrediente():
+    session = MagicMock(spec=Session)
+    ingrediente = Ingrediente(ingredienteID=1, nombre="Azúcar", cantidad=0)
+    
+    session.query().filter_by().first.return_value = ingrediente
+    
+    resultado = abastecer_ingrediente(session, 1, 50)
+    
+    assert ingrediente.cantidad == 50
+    session.commit.assert_called_once()
+    assert resultado == "Ingrediente Azúcar reabastecido con 50 unidades."
+
+def test_abastecer_ingrediente_suficiente_stock():
+    session = MagicMock(spec=Session)
+    ingrediente = Ingrediente(ingredienteID=1, nombre="Leche", cantidad=10)
+    
+    session.query().filter_by().first.return_value = ingrediente
+    
+    resultado = abastecer_ingrediente(session, 1, 50)
+    
+    assert ingrediente.cantidad == 10
+    assert resultado == "El ingrediente Leche aún tiene 10 unidades disponibles."
